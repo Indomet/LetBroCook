9c0e65f109e5aea57898c9e1d7fd6cce14498b92
Zepei Zhao <84900702+Zepeiz@users.noreply.github.com>
2023-09-07 16:13:02 +0200

    Add email validation and get all user request

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,26 +1,37 @@
 var mongoose = require("mongoose")
 var Schema = mongoose.Schema
 
+var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+var validateEmail = function(email) {
+    return regex.test(email)
+};
+
 var userSchema= new Schema(
     {
-        userName:  {
-            type: String, 
+        username:  {
+            type: String,
             required: true
         },
         email: {
-            type: String, 
-            required: true
+            type: String,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            required: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [regex, 'Please fill a valid email address']
         },
         password: {
-            type: String, 
+            type: String,
             required: true
         },
         name: {
-            type: String, 
+            type: String,
             required: true
         },
         recipes: {
-            type: [Schema.Types.ObjectId], 
+            type: [Schema.Types.ObjectId],
             ref: 'recipes'
         }
     }
