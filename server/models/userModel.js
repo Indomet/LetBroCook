d4df1db6513186f7d6ad635451c49b8d38581f12
almuslim <almuslim@net.chalmers.se>
2023-09-10 11:42:07 +0200

    add pasword hashing

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,14 @@
 var mongoose = require("mongoose")
 var Schema = mongoose.Schema
+var bcrypt = require("bcrypt")
+
+
+
+//salt work factor helps against rainbow table attacks if someone gets access of db
+//so this makes the key setup more expensive to combat server ddos
+const SALT_WORK_FACTOR = 10;
+
+
 
 var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
@@ -42,6 +51,35 @@ var userSchema= new Schema(
         
     }
 )
+
+//middleware function to has password when a suer document is saved
+//the hook event aka when the function is called is when saving to the db
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 const userModel = mongoose.model('users', userSchema);
 module.exports = userModel
