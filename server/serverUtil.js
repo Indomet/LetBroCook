df6920d26ec41b1251a72a491ab1450ee51d384d
Zepei Zhao <84900702+Zepeiz@users.noreply.github.com>
2023-09-13 22:50:37 +0200

    refactored code into routes and fix bug

--- server/serverUtil.js
+++ server/serverUtil.js
@@ -1,5 +1,30 @@
 regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    exports.validateEmail = function(email) {
-        return regex.test(email)
-    };
+    const validateEmail = function(email) {
+    return regex.test(email)
+};
+
+
+
+//  handle existing tags
+const handleExistingTags = async (tags) => {
+    const formattedTags = [];
+
+    for (const element of tags) {
+      let existingTag = await Tag.findOne({ name: element });
+
+      if (!existingTag) {
+        existingTag = new Tag({ name: element });
+        await existingTag.save();
+      }
+
+      formattedTags.push(existingTag);
+    }
+
+    return formattedTags;
+  };
+
+module.exports = {
+    validateEmail,
+    handleExistingTags
+}
