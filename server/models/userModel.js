a8885b3e16dee1660c9c58edfbb00782dfd6e153
Ali <almuslim@student.chalmers.se>
2023-09-18 18:07:19 +0000

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -71,6 +71,12 @@ userSchema.pre('save', function(next) {
     });
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 userSchema.methods.validateEmail = validateEmail
 
