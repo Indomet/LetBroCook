294f457159c5882f8b6c35d69c14f2df3b858237
Indomet <mehmetasim35@hotmail.com>
2023-09-23 11:34:30 +0200

    add signup page and connect it with backend

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -3,6 +3,9 @@
     <!-- href kills the state of the current page where routerLink doesnt lose the state of the page
     <router-link to="/login" class="auth-button">Login</router-link>
     <router-link to="/signup" class="auth-button">Signup</router-link> -->
+    <div id="home-button">
+      <router-link class="auth-button" to="/">Home(logohere)</router-link>
+    </div>
     <a href="/signup" class="auth-button">Signup</a>
     <a href="/login" class="auth-button">Login</a>
   </div>
@@ -15,6 +18,10 @@ export default {
 </script>
 
 <style scoped>
+#home-button, auth-button{
+  margin-right: auto;
+  padding: 5px 15px;
+}
 .long-bar {
   display: flex;
   background-color: #333;
