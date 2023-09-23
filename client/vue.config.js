88b1eab5148381bb5e13149be000f24cc20bd9c6
AliMousa27 <jafar.myhero@gmail.com>
2023-09-23 10:38:37 +0200

    migrate bootstrap, axios and vue to latest verisons

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  configureWebpack: {
-    devtool: 'source-map'
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
   }
 }
