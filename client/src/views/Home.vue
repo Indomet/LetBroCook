181cc90f89750537277d4800f77e018f8e679ba0
Indomet <mehmetasim35@hotmail.com>
2023-09-24 20:59:46 +0200

    update navigation bar

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,35 +1,4 @@
 <template>
-  <div>
-    <b-jumbotron header="DIT342 Frontend" lead="Welcome to your DIT342 Frontend Vue.js App">
-      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
-      <p v-if="user">Message from the server:<br/>
-      {{ message }}</p>
-    </b-jumbotron>
+  <div class="container-fluid">
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import { Api } from '@/Api'
-
-export default {
-  name: 'home',
-  methods: {
-    getMessage() {
-      Api.get('/')
-        .then(response => {
-          this.message = response.data.message
-        })
-        .catch(error => {
-          this.message = error
-        })
-    }
-  }
-}
-</script>
-
-<style>
-.btn_message {
-  margin-bottom: 1em;
-}
-</style>
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,32 +1,32 @@
 <template>
-    <div>
-      <form @submit.prevent="handleSubmit" class="signup-form">
-        <h3>Create an account</h3>
-        <div class="form-group">
-          <label for="username">Username</label>
-          <input type="username" id="username" v-model="username" class="form-control" />
-          <label for="name">Name</label>
-          <input type="name" id="name"  v-model="name" class="form-control" />
+    <div class="signup-container">
+          <form @submit.prevent="handleSubmit" class="signup-form">
+            <h3>Create an account</h3>
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="username" id="username" v-model="username" class="form-control" />
+              <label for="name">Name</label>
+              <input type="name" id="name" v-model="name" class="form-control" />
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" id="email" v-model="email" class="form-control" />
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" id="password" v-model="password" class="form-control" />
+              <label for="password_confirmation">Password confirmation</label>
+              <input type="password_confirmation" id="password_confirmation" v-model="password_confirmation" class="form-control" />
+            </div>
+            <button type="submit" class="btn btn-block">Signup</button>
+          </form>
+          <div class="divider">
+            <span class="divider-text">Do you have an account?</span>
+          </div>
+          <div class="account-button-container">
+            <a href="/login" class="btn btn-block">Login</a>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" id="email" v-model="email" class="form-control" />
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" id="password"  v-model="password" class="form-control" />
-          <label for="password_confirmation">Password confirmation</label>
-          <input type="password_confirmation" id="password_confirmation"  v-model="password_confirmation" class="form-control" />
-        </div>
-        <button type="submit" class="btn btn-block">Signup</button>
-      </form>
-      <div class="divider">
-          <span class="divider-text">New to our community</span>
-        </div>
-        <div class="account-button-container">
-        <a href="/signup" class="btn btn-block">Create an account</a>
-      </div>
-    </div>
 </template>
 
 <script>
@@ -65,67 +65,73 @@ export default {
 </script>
 
 <style scoped>
+
+.vegetables {
+  max-width: 60%;
+  margin: -200px -200px 0px;
+}
+
 .signup-form {
-    /* box size */
-    max-width: 400px;
-    /* 0  auto means no space and left - right sides are auto calculated if u wanna put somethign on right side it is margin-left: auto; */
-    margin: 0 auto;
-    /* padding is the distance between box and eements inside */
-    padding: 20px;
-    background-color: #ffffff;
-    border: 1px solid #f1f1f1;
-    border-radius: 20px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  /* box size */
+  max-width: 400px;
+  /* 0  auto means no space and left - right sides are auto calculated if u wanna put somethign on right side it is margin-left: auto; */
+  margin: 60px auto;
+  /* padding is the distance between box and eements inside */
+  padding: 20px;
+  background-color: #ffffff;
+  border: 1px solid #f1f1f1;
+  border-radius: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .form-group {
-    /* Space between form groups */
-    margin-bottom: 15px;
+  /* Space between form groups */
+  margin-bottom: 15px;
 }
 
 label {
-    font-weight: bold;
-    text-align: left;
-    display: block;
-    margin-bottom: 0px;
+  font-weight: bold;
+  text-align: left;
+  display: block;
+  margin-bottom: 0px;
 }
 
 .form-control {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    border-radius: 15px;
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 15px;
 }
 
 .btn {
-    padding: 10px 20px;
-    font-size: 16px;
-    border-radius: 40px;
-    border: 1px solid #111;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 40px;
+  border: 1px solid #111;
 }
 
 .divider {
-    max-width: 400px;
-    margin: 20px auto;
-    width: 100%;
-    height: 1px;
-    /* Adjust the thickness of the divider */
-    background-color: #ccc;
-    /* Adjust the color of the divider */
-
+  max-width: 400px;
+  margin: 20px auto;
+  width: 100%;
+  height: 1px;
+  /* Adjust the thickness of the divider */
+  background-color: #ccc;
+  /* Adjust the color of the divider */
 }
 
 .divider-text {
-    display: inline-block;
-    background-color: #fff;
-    padding: 0 10px;
-    position: relative;
-    top: -10px;
+  display: inline-block;
+  background-color: #fff;
+  padding: 0 10px;
+  position: relative;
+  top: -10px;
 }
 
 .account-button-container {
-    max-width: 400px;
-    margin: 0 auto;
-    text-align: center;
-}</style>
+  max-width: 400px;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
