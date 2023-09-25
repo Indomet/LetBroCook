7570c87997faeb66af285dec3c53003a8b5eb1ba
ZepeiZ <84900702+Zepeiz@users.noreply.github.com>
2023-09-25 22:38:41 +0200

    Add axios request on page load to get all recipes and vue compoenent to generate one card per recipe

--- client/src/views/Recipe.vue
+++ client/src/views/Recipe.vue
@@ -1,29 +1,88 @@
 <template>
     <div id="card">
-        <div class="main">
-            <img class="thumbnail" v-bind:src='thumbnail' alt="Thumbnail Image">
-            <div class="title">{{ title }}</div>
-            <div class="tags" v-for="tag in tags" v-bind:key="tag">
-                <div class="tag">{{ tag }}</div>
+        <div v-for="(recipe, i) in recipeData" v-bind:key="recipe.id">
+            <div v-if="i < 20">
+                <div class="main grid">
+                    <img class="thumbnail" v-bind:src='recipe.image' alt="Thumbnail Image">
+                    <p class="title">{{ recipe.title }}</p>
+                    <div v-for="(tag, j) in recipe.tags" v-bind:key="tag">
+                        <div v-if="j < 5">
+                            <div class="tags">{{ getTag(tag) }}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <!-- <div class="main">
+                <img class="thumbnail" v-bind:src='thumbnail' alt="Thumbnail Image">
+                <div class="title">{{ recipe.title }}</div>
+                <div class="tags" v-for="tag in tags" v-bind:key="tag">
+                    <div class="tag">{{ tag }}</div>
+                </div>
+            </div>
+            <p>{{ recipeData }}</p>-->
         </div>
     </div>
 </template>
 
+<script>
+import axios from 'axios'
+export default {
+    el: '#card',
+    mounted() {
+        axios.get('http://localhost:3000/v1/recipes')
+        .then((response) => {
+            this.recipeData = response.data.recipes
+        })
+        .catch((err) => {
+            console.log(err)
+            this.error = true
+        })
+        .finally(() => {
+            this.loading = false
+        })
+    },
+    data() {
+        return {
+            recipeData: '',
+            loading: true,
+            error: false,
+            title: 'Kitsune Chan',
+            thumbnail: 'https://mangadex.org/covers/a914a65b-27ed-4665-9767-87396a00ea6b/9298804f-54e7-495e-afcb-70fdd7d25bf0.jpg',
+            description: 'df',
+            tags: ['tag1', 'tag22', 'tag333', 'tag4444', 'tag55555']
+        }
+    },
+    methods: {
+        async getTag(tagId) {
+            try {
+                const response = await axios.get('http://localhost:3000/v1/recipes/tags/' + tagId)
+                const tagName = response.data.name
+                return tagName
+            } catch (error) {
+                console.error('Error fetching tag:', error)
+                throw error
+            }
+        }
+    }
+}
+</script>
+
 <style>
 .main{
     min-height: 30vw;
-    height: auto;
     width: 20vw;
     margin-left: 10%;
-    margin-right: 10%;
     margin-top: 5%;
     background-color: antiquewhite;
-    outline-width: 2px;
+    outline-width: 0.13vw;
     outline-color: rgb(159, 159, 159);
     outline-style:solid;
-    border-radius: 5px;
-    position: relative;
+    border-radius: 1rem;
+    float: left;
+    clear: none;
+    text-align: center;
+    position: center;
+    grid-row: 3;
 }
 
 .main:hover {
@@ -48,11 +107,12 @@
     aspect-ratio: 1/1;
     object-fit: cover;
     object-position: center;
-    border-radius: 0.65vw;
+    border-radius: 1rem;
+    position: relative;
 }
 
 .thumbnail:hover{
-    outline-width: 2px;
+    outline-width: 0.13vw;
     outline-color: rgb(27, 18, 21);
     outline-style: inset;
 }
@@ -65,14 +125,14 @@
     display: inline-block;
     overflow-wrap: break-word;
     background-color: rgb(145, 145, 239);
-    font-size: 1.5vw;
+    font-size: 1.2vw;
     padding: 0.325vw;
     margin-left: 1vw;
     margin-right: 0.65vw;
     margin-bottom: 1vw;
     border-radius: 1rem;
     cursor: pointer;
-    outline-width: 2px;
+    outline-width: 0.13vw;
     outline-color: rgb(27, 18, 21);
     outline-style: solid;
 }
@@ -81,21 +141,3 @@
 }
 
 </style>
-
-<script>
-export default {
-    el: '#card',
-    methods: {
-        onHover() {
-        }
-    },
-    data() {
-        return {
-            title: 'Kitsune Chan',
-            thumbnail: 'https://mangadex.org/covers/a914a65b-27ed-4665-9767-87396a00ea6b/9298804f-54e7-495e-afcb-70fdd7d25bf0.jpg',
-            description: 'df',
-            tags: ['tag1', 'tag22', 'tag333', 'tag4444', 'tag55555']
-        }
-    }
-}
-</script>
