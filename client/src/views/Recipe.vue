1c95959f94c402d5462b6e54de2e7dbeb740bebf
ZepeiZ <84900702+Zepeiz@users.noreply.github.com>
2023-10-02 16:19:02 +0200

    Fix UI with responsive flip cards

--- client/src/views/Recipe.vue
+++ client/src/views/Recipe.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="d-flex flex-wrap" style="margin-inline-start: 4rem;">{{ trimArrayList(recipeData) }}
-        <div v-for="(recipe) in recipeArray" :key="recipe.id" class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 d-flex"
-            style="margin-bottom: 6rem; margin-right: -2rem;">
-            <!--The card-->
-            <div class="main-container">
-                <div :class="{ 'flipped': recipe.flipped, 'flip-card': 'flip-card', 'w-100': 'w-100' }">
-                    <!--Front side-->
-                    <div class="front card flex-fill h-100" style="max-width: 20rem; border-radius: 1rem;
+    <div>
+        <div class="d-flex flex-wrap">
+            {{ trimArrayList(recipeData) }}
+            <div v-for="(recipe) in recipeArray" :key="recipe.id" class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 d-flex"
+                style="margin-bottom: 4rem;">
+                <div class="main-container">
+                <div :class="{ 'flipped': recipe.flipped, 'card': 'card', 'w-100': 'w-100'}" style="max-width: 20rem; border-radius: 1rem;
               background-image: linear-gradient(45deg, #d8e8dc 12.50%, #ffffff 12.50%, #ffffff 25%, #f0f0f0 25%, #f0f0f0 50%, #d8e8dc 50%, #d8e8dc 62.50%, #ffffff 62.50%, #ffffff 75%, #f0f0f0 75%, #f0f0f0 100%);
 background-size: 40.00px 40.00px;">
-                        <div @click="flipCard(recipe)" class="flip-button">Click to flip</div>
+                    <b-card-body class="front">
                         <img class="card-image" b-card-img-top :src="recipe.image" alt="Thumbnail Image">
                         <p class="card-title">{{ recipe.title }}</p>
-                        <div>{{ trimTagList(recipe.tags) }}
+                        <div class="tag-block">{{ trimTagList(recipe.tags) }}
                             <span v-for="(tag) in tagArray" :key="tag" class="tags">
                                 {{ tag.name }}
                             </span>
                         </div>
-                        <h5>{{ flipped }}</h5>
-                    </div>
-                    <!--Back side-->
-                    <div class="back card flex-fill h-100" style="max-width: 20rem; border-radius: 1rem;
-              background-image: linear-gradient(135deg, #d8e8dc 12.50%, #ffffff 12.50%, #ffffff 25%, #f0f0f0 25%, #f0f0f0 50%, #d8e8dc 50%, #d8e8dc 62.50%, #ffffff 62.50%, #ffffff 75%, #f0f0f0 75%, #f0f0f0 100%);
-background-size: 40.00px 40.00px;">
-                        <div @click="flipCard(recipe)" class="flip-button">Click to flip back</div>
+                        <div @click="flipCard(recipe)" class="flip-button">More info</div>
+                    </b-card-body>
+
+                    <b-card-body class="back">
                         <h5 class="card-description">{{ recipe.steps }}</h5>
-                    </div>
+                        <div @click="flipCard(recipe)" class="flip-button">Back</div>
+                    </b-card-body>
                 </div>
             </div>
-        </div>
+                </div>
+            </div>
     </div>
 </template>
 
@@ -83,7 +81,7 @@ export default {
             }
         },
         trimArrayList(arr) {
-            const end = 7 // Max number of tags to be shown
+            const end = 10 // Max number of tags to be shown
             if (arr.length > end) {
                 const newArr = arr.slice(0, end)
                 this.recipeArray = newArr
@@ -94,30 +92,33 @@ export default {
 </script>
 
 <style scoped>
+
 .main-container {
-    perspective: 1000px;
+
     position: relative;
-    width: 40vh;
-    height: 30vw;
+
 }
 
-.flip-card {
-    position: absolute;
-    width: 100%;
-    height: 100%;
+.card {
+    perspective: 1000px;
     transform-style: preserve-3d;
     transition: all 0.5s ease-in-out;
 }
 
+.card:hover {
+    outline-width: 3px;
+    outline-color: rgb(255, 255, 255);
+    outline-style: solid;
+}
+
 .flipped {
     transform: rotateY(-180deg);
-    scale: 1.5;
 }
 
 .front {
     width: 100%;
     height: 100%;
-    position: absolute;
+    position: relative;
     backface-visibility: hidden;
 }
 
@@ -126,25 +127,37 @@ export default {
     height: 100%;
     position: absolute;
     backface-visibility: hidden;
-    background-color: aqua;
     transform: rotateY(180deg);
-    overflow-y: scroll;
     z-index: 2;
+    overflow-y: scroll;
 }
 
 .card-description {
-    font-size: small;
+    font-size: medium;
 }
 
 .flip-button {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    cursor: pointer;
+    font-weight: bold;
+    color: white;
+    width: 50%;
     outline-width: 1px;
     outline-color: black;
     outline-style: solid;
-    opacity: 0;
+    cursor: pointer;
+    opacity: 50;
+    border-radius: 1rem;
+    background-color: inherit;
+    background-color: rgb(0, 92, 163);
+    position: static;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: auto;
+    margin-bottom: 0;
+}
+
+.flip-button:hover {
+    outline-color: white;
+    background-color: rgb(29, 130, 207);
 }
 
 .card-title {
@@ -172,10 +185,13 @@ export default {
 
 .card-image:hover {
     outline-width: 0.13vw;
-    outline-color: rgb(27, 18, 21);
+    outline-color: rgb(245, 245, 245);
     outline-style: inset;
 }
 
+.tag-block {
+    margin-bottom: 20px;
+}
 .tags {
     background-color: rgb(160, 182, 244);
     border-radius: 1rem;
@@ -183,7 +199,7 @@ export default {
     outline-width: 10%;
     outline-color: rgb(27, 18, 21);
     outline-style: solid;
-    font-size: 16px;
+    font-size: 14px;
     padding: 4px;
     margin: 5px;
     display: inline-block;
@@ -191,5 +207,32 @@ export default {
 
 .tags:hover {
     background-color: rgb(96, 110, 219);
+}
+
+/* Scrollbar styling */
+
+/* width */
+::-webkit-scrollbar {
+  width: 11px;
+  margin-right: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #a2a2a2;
+  border-radius: 1rem;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #8dcc91;
+  border-radius: 1rem;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #3a8e66;
 }
 </style>
