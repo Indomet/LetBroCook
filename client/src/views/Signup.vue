c5227381918f19ff50944f9011ab29dd4a557aa7
Indomet <mehmetasim35@hotmail.com>
2023-10-14 14:33:18 +0200

    edit css styles and get rid of unused styles

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,79 +1,79 @@
 <template>
-    <div class="container h-100" >
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-lg-12 col-xl-11">
-          <div class="card text-black" style="border-radius: 25px;">
-            <div class="card-body p-md-5">
-              <div class="row justify-content-center">
-                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                  <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-                  <form @submit.prevent="handleSubmit" class="mx-1 mx-md-4">
-                    <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                      <div class="form-outline flex-fill mb-0">
-                        <label class="form-label" for="username">Username</label>
-                        <input type="username" id="username" v-model="username" class="form-control"
-                          :class="{ 'is-invalid': getError('username') }" />
-                        <div v-if="getError('username')" class="invalid-feedback">{{ getError('username') }} </div>
-                        <div v-if="usernameError" class="invalid-feedback d-block">{{ usernameError }}</div>
-                      </div>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card" style="border-radius: 35px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 order-lg-1 col-xl-5 order-2">
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                <form @submit.prevent="handleSubmit" class="mx-1 mx-md-4">
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="flex-fill mb-0">
+                      <label class="form-label" for="username">Username</label>
+                      <input type="username" id="username" v-model="username" class="form-control"
+                        :class="{ 'is-invalid': getError('username') }" />
+                      <div v-if="getError('username')" class="invalid-feedback">{{ getError('username') }} </div>
+                      <div v-if="usernameError" class="invalid-feedback d-block">{{ usernameError }}</div>
                     </div>
-                    <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                      <div class="form-outline flex-fill mb-0">
-                        <label for="name">Name</label>
-                        <input type="name" id="name" v-model="name" class="form-control"
-                          :class="{ 'is-invalid': getError('name') }" />
-                        <div v-if="getError('name')" class="invalid-feedback">{{ getError('name') }} </div>
-                      </div>
+                  </div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="flex-fill mb-0">
+                      <label for="name">Name</label>
+                      <input type="name" id="name" v-model="name" class="form-control"
+                        :class="{ 'is-invalid': getError('name') }" />
+                      <div v-if="getError('name')" class="invalid-feedback">{{ getError('name') }} </div>
                     </div>
-                    <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                      <div class="form-outline flex-fill mb-0">
-                        <label class="form-label" for="email">Email</label>
-                        <input type="email" id="email" v-model="email" class="form-control"
-                          :class="{ 'is-invalid': getError('email')}" />
-                        <div v-if="getError('email')" class="invalid-feedback">{{ getError('email') }} </div>
-                        <div v-if="emailError"  class="invalid-feedback d-block">{{ emailError }}</div>
-                      </div>
+                  </div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="flex-fill mb-0">
+                      <label class="form-label" for="email">Email</label>
+                      <input type="email" id="email" v-model="email" class="form-control"
+                        :class="{ 'is-invalid': getError('email') }" />
+                      <div v-if="getError('email')" class="invalid-feedback">{{ getError('email') }} </div>
+                      <div v-if="emailError" class="invalid-feedback d-block">{{ emailError }}</div>
                     </div>
-                    <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                      <div class="form-outline flex-fill mb-0">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" v-model="password" class="form-control"
-                          :class="{ 'is-invalid': getError('password') }" />
-                        <div v-if="getError('password')" class="invalid-feedback">{{ getError('password') }} </div>
-                      </div>
+                  </div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="flex-fill mb-0">
+                      <label for="password">Password</label>
+                      <input type="password" id="password" v-model="password" class="form-control"
+                        :class="{ 'is-invalid': getError('password') }" />
+                      <div v-if="getError('password')" class="invalid-feedback">{{ getError('password') }} </div>
                     </div>
-                    <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                      <div class="form-outline flex-fill mb-0">
-                        <label for="password_confirmation">Password confirmation</label>
-                        <input type="password" id="password_confirmation" v-model="password_confirmation"
-                          class="form-control" :class="{ 'is-invalid': getError('password_confirmation') }" />
-                        <div v-if="getError('password_confirmation')" class="invalid-feedback">{{
-                          getError('password_confirmation') }} </div>
-                      </div>
+                  </div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div class="flex-fill mb-0">
+                      <label for="password_confirmation">Password confirmation</label>
+                      <input type="password" id="password_confirmation" v-model="password_confirmation"
+                        class="form-control" :class="{ 'is-invalid': getError('password_confirmation') }" />
+                      <div v-if="getError('password_confirmation')" class="invalid-feedback">{{
+                        getError('password_confirmation') }} </div>
                     </div>
-                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                      <button type="submit" class="btn btn-primary btn-block">Signup</button>
-                    </div>
-                  </form>
-                  <div class="divider">
-                    <span class="divider-text">Do you have an account?</span>
                   </div>
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <a href="/login" class="btn btn-primary btn-lg">Login</a>
+                    <button type="submit" class="btn btn-primary btn-block">Signup</button>
                   </div>
+                </form>
+                <div class="divider">
+                  <span class="divider-text">Do you have an account?</span>
                 </div>
-                <div class="vegetables col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                  <img src="../assets/vegetables.png" class="img-fluid" alt="Vegatables">
+                <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                  <a href="/login" class="btn btn-primary btn-lg">Login</a>
                 </div>
+              </div>
+              <div class="vegetables col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                <img src="../assets/vegetables.png" class="img-fluid" alt="Vegatables">
               </div>
             </div>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -167,9 +167,6 @@ export default {
 </script>
 
 <style scoped>
-.paint-red{
-  color: red;
-}
 .vegetables {
   max-width: 47%;
 }
@@ -190,9 +187,10 @@ label {
   max-width: 400px;
   margin: 40px auto 30px;
   width: 100%;
-    /* thickness of the divider */
+  /* thickness of the divider */
   height: 1px;
   background-color: #ccc;
+  /* margin-bottom: 40px; */
 }
 
 .divider-text {
@@ -209,5 +207,4 @@ label {
   border-radius: 40px;
 
 }
-
 </style>
